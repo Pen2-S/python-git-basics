9cb4b7a52a44edb7145fc13a075592d8b4131f0a
Vladislav Romanovskiy <lolkekmem820@gmail.com>
2026-01-21 21:32:29 +0100

    TASK 3: 3 code of mine

--- 03_conditions.py
+++ 03_conditions.py
@@ -11,12 +11,19 @@ Notes:
 
 # TODO 1:
 # Ask the user for their age (as int)
-
-
+print("What's your age?: \n")
+age = input()
+age = int(age)
 # TODO 2:
 # If age >= 18 print "You are an adult"
 # Otherwise print "You are a minor"
 
+if age >= 65:
+    print("Still alive?")
+elif age >= 18:
+    print("Big bro")
+else:
+    print("Small, grow up")
 
 # TODO 3 (optional):
 # Add another condition:
